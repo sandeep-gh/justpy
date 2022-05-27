d18a44cc7570980b6cd7a5b3bef7061a18728197
Kabira <kabira@monallabs.in>
2022-05-27 16:54:17 +0530

    adding svelte bundle framework from https://github.com/Los-Crackitos/svelte-tailwind-i18n-template

---
+++ justpy/templates/js/svelte/src/Htmlcomponent.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let jp_props;
+  let description_object = {
+    style: jp_props.style,
+    attrs: jp_props.attrs
+  };
+  if (jp_props.classes) {
+    description_object['class'] = jp_props.classes
+  }
+  console.log(description_object);
+  console.log("from htmlcomponents");
+  console.log(jp_props);
+  console.log("hello");
+</script>
+
+
+<svelte:element this={jp_props.html_tag} {...description_object}>
+{#if jp_props.text}
+{jp_props.text}
+{/if}
+{#each jp_props.object_props as cobj_props}
+{#if cobj_props.show}
+<svelte:self jp_props={cobj_props}/>
+{/if}
+{/each}
+
+</svelte:element>
---
+++ justpy/templates/js/svelte/src/main.js
@@ -0,0 +1,89 @@
+import { init, getLocaleFromNavigator } from "svelte-i18n";
+import App from "./App.svelte";
+import "./Tailwind.svelte";
+import "./i18n";
+
+const fallbackLocale = "en-US";
+const navigatorLocale = getLocaleFromNavigator();
+
+init({
+  fallbackLocale,
+  initialLocale: ["en-US", "fr-FR"].includes(navigatorLocale)
+    ? navigatorLocale
+    : fallbackLocale,
+});
+
+var justpyComponents =[{
+    "attrs": {},
+    "id": null,
+    "vue_type": "html_component",
+    "show": true,
+    "events": [],
+    "event_modifiers": {},
+    "classes": "bg-pink-100",
+    "style": "",
+    "set_focus": false,
+    "html_tag": "div",
+    "class_name": "Div",
+    "event_propagation": true,
+    "inner_html": "",
+    "animation": false,
+    "debug": false,
+    "transition": null,
+    "directives": {},
+    "scoped_slots": {},
+    "object_props": [{
+        "attrs": {},
+        "id": null,
+        "vue_type": "html_component",
+        "show": true,
+        "events": [],
+        "event_modifiers": {},
+        "classes": "",
+        "style": "color: blue; font-size: 30px",
+        "set_focus": false,
+        "html_tag": "button",
+        "class_name": "Button",
+        "event_propagation": true,
+        "inner_html": "",
+        "animation": false,
+        "debug": false,
+        "transition": null,
+        "directives": {},
+        "scoped_slots": {},
+        "object_props": [],
+        "text": "abtn"
+    }, {
+        "attrs": {},
+        "id": null,
+        "vue_type": "html_component",
+        "show": true,
+        "events": [],
+        "event_modifiers": {},
+        "classes": "",
+        "style": "color: blue; font-size: 30px",
+        "set_focus": false,
+        "html_tag": "button",
+        "class_name": "Button",
+        "event_propagation": true,
+        "inner_html": "",
+        "animation": false,
+        "debug": false,
+        "transition": null,
+        "directives": {},
+        "scoped_slots": {},
+        "object_props": [],
+        "text": "abtn2"
+    }],
+    "text": "outer dive"
+}];
+const app = new App({
+  target: document.body,
+  props: {
+      name: "world",
+    atag: "span",
+    justpyComponents : justpyComponents
+  },
+});
+
+export default app;
